8bd3c9714fae71ec7db3163307054b8feb980ffe
Hector Galindo <hectordaniel@gmail.com>
2022-01-14 09:55:40 -0500

    elaborando mi portafolio

---
+++ Ahorcadito/script.js
@@ -0,0 +1,12 @@
+let siguiente = document.getElementById('Derecha')
+let anterior = document.getElementById('Izquierda')
+
+
+
+function derecha(){
+    if(let = siguiente)
+    {
+        window.location=""
+    }
+
+}
